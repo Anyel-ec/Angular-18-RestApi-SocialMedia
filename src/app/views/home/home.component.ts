254594d1fd1d19099d67c05b92218b9868b7d639
anyel-ec <paulangel826@gmail.com>
2024-06-21 00:19:27 -0500

    chore: Refactor comment section in home component

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -64,10 +64,40 @@ export class HomeComponent implements OnInit {
 
     this.loadPosts();
   }
+  // Cancelar la edición de un comentario
+  cancelEditComment(comment: any): void {
+    comment.isEditing = false;
+  }
+  // Iniciar la edición de un comentario
+  startEditComment(comment: any): void {
+    comment.isEditing = true;
+    comment.editContent = comment.content;
+  }
 
-
-
-
+  saveEditedComment(comment: any): void {
+    this.commentService.updateComment(comment.id, { ...comment, content: comment.editContent }).subscribe(
+      response => {
+        // Actualización exitosa
+        console.log('Comentario actualizado:', response);
+        comment.content = response.content;
+        comment.isEditing = false;
+        Swal.fire({
+          icon: 'success',
+          title: 'Comentario actualizado',
+          text: 'El comentario se ha actualizado correctamente.'
+        });
+      },
+      error => {
+        // Error al actualizar
+        console.error('Error al actualizar comentario:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Hubo un problema al actualizar el comentario. Por favor, intenta nuevamente.'
+        });
+      }
+    );
+  }
   addComment(): void {
     if (this.selectedPublication && this.newComment.trim()) {
       const comment = {
@@ -212,26 +242,6 @@ export class HomeComponent implements OnInit {
 
 
 
-  resetPostForm(): void {
-    this.post = {
-      title: '',
-      content: '',
-      category_id: null,
-      usuario_id: this.user.id,
-      time_created: ''
-    };
-  }
-
-  loadUserName(userId: number): void {
-    this.usersService.getUserById(userId).subscribe(
-      userResponse => {
-        this.userMap[userId] = userResponse.name;
-      },
-      error => {
-        console.error(`Error fetching user with id ${userId}:`, error);
-      }
-    );
-  }
 
 
 
@@ -288,187 +298,211 @@ export class HomeComponent implements OnInit {
     }
   }
 
-//////////////////////////////////////////////////
-loadPosts(): void {
-  this.postService.getPosts().subscribe(
-    response => {
-      this.publications = response;
-      this.publications.forEach(pub => {
-        pub.comments = pub.comments || []; // Asegurar que comments esté inicializado como un arreglo vacío si no existe
-        this.loadCommentsForPublication(pub); // Cargar comentarios para cada publicación
-        this.usersService.getUserById(pub.usuario_id).subscribe(
-          userResponse => {
-            this.userMap[pub.usuario_id] = userResponse.name;
-          },
-          error => {
-            console.error(`Error fetching user with id ${pub.usuario_id}:`, error);
-          }
-        );
-      });
-    },
-    error => {
-      console.error('Error fetching posts:', error);
-    }
-  );
-}
+  //////////////////////////////////////////////////
 
-loadPublications(): void {
-  this.postService.getPosts().subscribe(
-    response => {
-      this.publications = response;
-      this.publications.forEach(pub => {
-        pub.comments = pub.comments || [];
-        this.loadUserName(pub.usuario_id);
-      });
-    },
-    error => {
-      console.error('Error fetching posts:', error);
-    }
-  );
-}
-
-
-
-onEditSubmit(event: Event, publication: any): void {
-  event.preventDefault();
-
-  this.postService.updatePost(publication.id, publication).subscribe(
-    response => {
-      Swal.fire({
-        icon: 'success',
-        title: 'Publicación actualizada',
-        text: 'Los cambios se han guardado correctamente.',
-        showConfirmButton: false,
-        timer: 1500,
-        position: 'top-end',
-        toast: true
-      });
-
-      this.cancelEdit();
-      this.loadPublications();
-    },
-    error => {
-      console.error('Error al actualizar la publicación', error);
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Hubo un problema al guardar los cambios. Por favor, intenta nuevamente.',
-        showConfirmButton: false,
-        timer: 1500
-      });
-    }
-  );
-}
-onPostSubmit(event: Event): void {
-  event.preventDefault();
-  this.post.time_created = new Date().toISOString();
-
-  this.postService.createPost(this.post).subscribe(
-    response => {
-      Swal.fire({
-        icon: 'success',
-        title: 'Post creado',
-        text: 'Tu publicación ha sido creada exitosamente!',
-        timer: 1500,
-        showConfirmButton: false,
-        position: 'top-end',
-        toast: true
-      });
-      this.post.title = '';
-      this.post.content = '';
-      this.post.category_id = null;
-      this.loadPosts(); // Actualizar la lista de publicaciones después de crear una nueva
-    },
-    error => {
-      console.error('Error creando post:', error);
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Hubo un error al crear tu publicación. Por favor, intenta nuevamente.',
-        timer: 1500,
-        showConfirmButton: false,
-        position: 'top-end',
-        toast: true
-      });
-    }
-  );
-}
-
-toggleLike(publication: any): void {
-  publication.liked = !publication.liked;
-  if (publication.liked) {
-    publication.num_likes++;
-  } else {
-    publication.num_likes--;
+  resetPostForm(): void {
+    this.post = {
+      title: '',
+      content: '',
+      category_id: null,
+      usuario_id: this.user.id,
+      time_created: ''
+    };
   }
-  // Aquí puedes llamar a un servicio para guardar el estado del like en el backend si es necesario.
-}
+
+  loadUserName(userId: number): void {
+    this.usersService.getUserById(userId).subscribe(
+      userResponse => {
+        this.userMap[userId] = userResponse.name;
+      },
+      error => {
+        console.error(`Error fetching user with id ${userId}:`, error);
+      }
+    );
+  }
 
 
-toggleEditPublication(publication: any): void {
-  if (this.editingPublication && this.editingPublication.id === publication.id) {
-    this.editingPublication = null;
-  } else {
+
+  loadPosts(): void {
+    this.postService.getPosts().subscribe(
+      response => {
+        this.publications = response;
+        this.publications.forEach(pub => {
+          pub.comments = pub.comments || []; // Asegurar que comments esté inicializado como un arreglo vacío si no existe
+          this.loadCommentsForPublication(pub); // Cargar comentarios para cada publicación
+          this.usersService.getUserById(pub.usuario_id).subscribe(
+            userResponse => {
+              this.userMap[pub.usuario_id] = userResponse.name;
+            },
+            error => {
+              console.error(`Error fetching user with id ${pub.usuario_id}:`, error);
+            }
+          );
+        });
+      },
+      error => {
+        console.error('Error fetching posts:', error);
+      }
+    );
+  }
+
+  loadPublications(): void {
+    this.postService.getPosts().subscribe(
+      response => {
+        this.publications = response;
+        this.publications.forEach(pub => {
+          pub.comments = pub.comments || [];
+          this.loadUserName(pub.usuario_id);
+        });
+      },
+      error => {
+        console.error('Error fetching posts:', error);
+      }
+    );
+  }
+
+
+
+  onEditSubmit(event: Event, publication: any): void {
+    event.preventDefault();
+
+    this.postService.updatePost(publication.id, publication).subscribe(
+      response => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Publicación actualizada',
+          text: 'Los cambios se han guardado correctamente.',
+          showConfirmButton: false,
+          timer: 1500,
+          position: 'top-end',
+          toast: true
+        });
+
+        this.cancelEdit();
+        this.loadPublications();
+      },
+      error => {
+        console.error('Error al actualizar la publicación', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Hubo un problema al guardar los cambios. Por favor, intenta nuevamente.',
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    );
+  }
+  onPostSubmit(event: Event): void {
+    event.preventDefault();
+    this.post.time_created = new Date().toISOString();
+
+    this.postService.createPost(this.post).subscribe(
+      response => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Post creado',
+          text: 'Tu publicación ha sido creada exitosamente!',
+          timer: 1500,
+          showConfirmButton: false,
+          position: 'top-end',
+          toast: true
+        });
+        this.post.title = '';
+        this.post.content = '';
+        this.post.category_id = null;
+        this.loadPosts(); // Actualizar la lista de publicaciones después de crear una nueva
+      },
+      error => {
+        console.error('Error creando post:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Hubo un error al crear tu publicación. Por favor, intenta nuevamente.',
+          timer: 1500,
+          showConfirmButton: false,
+          position: 'top-end',
+          toast: true
+        });
+      }
+    );
+  }
+
+  toggleLike(publication: any): void {
+    publication.liked = !publication.liked;
+    if (publication.liked) {
+      publication.num_likes++;
+    } else {
+      publication.num_likes--;
+    }
+    // Aquí puedes llamar a un servicio para guardar el estado del like en el backend si es necesario.
+  }
+
+
+  toggleEditPublication(publication: any): void {
+    if (this.editingPublication && this.editingPublication.id === publication.id) {
+      this.editingPublication = null;
+    } else {
+      this.editingPublication = { ...publication };
+    }
+  }
+  openComments(publication: any): void {
+    this.selectedPublication = publication;
+    this.newComment = '';
+  }
+  editPublication(publication: any): void {
     this.editingPublication = { ...publication };
   }
-}
-openComments(publication: any): void {
-  this.selectedPublication = publication;
-  this.newComment = '';
-}
-editPublication(publication: any): void {
-  this.editingPublication = { ...publication };
-}
 
-deletePublication(publication: any): void {
-  Swal.fire({
-    title: '¿Estás seguro?',
-    text: "¡No podrás revertir esto!",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Sí, eliminarlo',
-    cancelButtonText: 'Cancelar'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      this.postService.deletePost(publication.id).subscribe(
-        response => {
-          console.log('Publicación eliminada', response);
-          // Elimina la publicación de la lista local después de eliminarla del servidor
-          this.publications = this.publications.filter(p => p.id !== publication.id);
-          // Mostrar alerta de éxito
-          Swal.fire({
-            icon: 'success',
-            title: 'Publicación eliminada',
-            text: 'La publicación ha sido eliminada con éxito.',
-            showConfirmButton: false,
-            timer: 1500
-          });
-        },
-        error => {
-          console.error('Error al eliminar la publicación', error);
-          // Mostrar alerta de error
-          Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: 'Hubo un problema al eliminar la publicación.',
-            showConfirmButton: false,
-            timer: 1500
-          });
-        }
-      );
-    }
-  });
-}
-
-toggleComments(publication: any): void {
-  if (this.selectedPublication === publication) {
-    this.selectedPublication = null;
-  } else {
-    this.selectedPublication = publication;
-    this.loadCommentsForPublication(publication);
+  deletePublication(publication: any): void {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "¡No podrás revertir esto!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminarlo',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.postService.deletePost(publication.id).subscribe(
+          response => {
+            console.log('Publicación eliminada', response);
+            // Elimina la publicación de la lista local después de eliminarla del servidor
+            this.publications = this.publications.filter(p => p.id !== publication.id);
+            // Mostrar alerta de éxito
+            Swal.fire({
+              icon: 'success',
+              title: 'Publicación eliminada',
+              text: 'La publicación ha sido eliminada con éxito.',
+              showConfirmButton: false,
+              timer: 1500
+            });
+          },
+          error => {
+            console.error('Error al eliminar la publicación', error);
+            // Mostrar alerta de error
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'Hubo un problema al eliminar la publicación.',
+              showConfirmButton: false,
+              timer: 1500
+            });
+          }
+        );
+      }
+    });
   }
-}
+
+  toggleComments(publication: any): void {
+    if (this.selectedPublication === publication) {
+      this.selectedPublication = null;
+    } else {
+      this.selectedPublication = publication;
+      this.loadCommentsForPublication(publication);
+    }
+  }
 
 }
